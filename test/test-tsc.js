a92bfa0c62da6f45056da7de680fe179bdd53d7a
sergeyt <stodyshev@gmail.com>
2013-11-16 00:19:01 +0700

    updated tsc module

--- test/test-tsc.js
+++ test/test-tsc.js
@@ -1,5 +1,8 @@
 var tsc = require('../lib/tsc.js');
-tsc(__dirname + '/hello.ts', __dirname + '/hello.js', {}, function(error, result){
-	if (error) throw error;
-	console.log(result);
-});
+tsc(__dirname + '/hello.ts', null,
+	{
+		sourceMap: true
+	}, function(error, result) {
+		if (error) throw error;
+		console.log(result);
+	});
